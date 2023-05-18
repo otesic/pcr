09494d401938506798439494d7f4d2e26612cb2d
parkcheolryeon <qkrcjffus@naver.com>
2023-05-19 08:34:25 +0900

    FEAT: canSkillPage navibar create

---
+++ src/components/canSkill/canSkill.tsx
@@ -0,0 +1,55 @@
+const CanSkill = () => {
+  return (
+    <>
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <span className="font-semibold text-xl tracking-tight">
+            가능한 스킬
+          </span>
+        </div>
+
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              Three.js
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              게시판 기능
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              소셜로그인
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              인피니티 스크롤
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              pdf 출력
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              카카오 맵 활용
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+export default CanSkill;
